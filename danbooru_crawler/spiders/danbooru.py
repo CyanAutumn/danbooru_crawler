8b852257951e709d8add3718d911c4ec7dbb40f7
茶居 <1196395297@qq.com>
2023-11-16 14:22:40 +0800

    🐞 fix: 不再抓取套图，因为套图可能没有所搜索的tag特征，一概抓取会污染训练集

--- danbooru_crawler/spiders/danbooru.py
+++ danbooru_crawler/spiders/danbooru.py
@@ -14,44 +14,30 @@ class DanbooruSpider(scrapy.Spider):
 
     def parse(self, response):
         """搜索页"""
+        # 详情页
         for _ in response.xpath('//a[@class="post-preview-link"]/@href').getall():
-            pic_page_url = parse.urljoin(self.url, _)
-            self.logger.info(f"图片详情页 {pic_page_url}")
-            yield scrapy.Request(
-                url=pic_page_url, callback=self.parse_pic_page_url, priority=2
-            )
+            self.logger.info(f"图片详情页 {parse.urljoin(self.url, _)}")
+            yield response.follow(_, callback=self.parse_pic_page_url, priority=2)
         # 下一页
         next_url = response.xpath('//a[@rel="next" and @href]/@href').get()
         if next_url is not None:
-            next_url = parse.urljoin(self.url, next_url)
             self.logger.info(f"下一页 {next_url}")
-            yield scrapy.Request(url=next_url, callback=self.parse, priority=3)
+            yield response.follow(next_url, callback=self.parse, priority=3)
 
     def parse_pic_page_url(self, response):
         """图片详情页"""
-        pic_page_url_list = response.xpath(
-            '//div/article/div/a[@class="post-preview-link"]/@href'
-        ).getall()
-        if len(pic_page_url_list) > 1:
-            pic_page_url_list = pic_page_url_list[1:]
-        self.logger.info(f"图片资源页 {response.url}")
-        yield self.parse_pic_url(response)
-        for _ in pic_page_url_list:
-            pic_page_url = parse.urljoin(self.url, _)
-            self.logger.info(f"套图url {pic_page_url}")
-            yield scrapy.Request(
-                url=pic_page_url, callback=self.parse_pic_url, priority=1
-            )
-
-    def parse_pic_url(self, response):
-        """推送"""
+        self.logger.info(f"图片详情页 {response.url}")
+        img_url = None
         if settings.SEARCH_TYPE == 0:
-            pic_url = response.xpath("//section/picture/source/@srcset").get()
+            img_url = response.xpath("//section/picture/source/@srcset").get()
         elif settings.SEARCH_TYPE == 1:
-            pic_url = response.xpath(
+            img_url = response.xpath(
                 '//li/a[@class="image-view-original-link"]/@href'
             ).get()
-        self.logger.info(f"图片url {pic_url}")
+        if img_url is None:
+            self.logger.info(f"抓取到空地址，可能是动图/视频")
+            return None
+        self.logger.info(f"图片url {img_url}")
         img_items = items.ImagedownloadItem()
-        img_items["image_urls"] = [pic_url]
+        img_items["image_urls"] = [img_url]
         return img_items
